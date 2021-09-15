069f61072b6bab51413c87a739c8e8772f1054bd
Ken Smith <ken.smith@shopify.com>
2021-09-15 16:58:09 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,4 +3,4 @@
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 cp ~/dotfiles/.zshrc ~/.zshrc
-cp ~/dotfiles/zsh_themes/robbyrussell_with_host.zsh-theme ~/.oh-my-zsh/custom/themes/robbyrussell_with_host.zsh-theme
+cp ~/.oh-my-zsh/themes/robbyrussell.zsh-theme ~/.oh-my-zsh/custom/themes/robbyrussell.zsh-theme
